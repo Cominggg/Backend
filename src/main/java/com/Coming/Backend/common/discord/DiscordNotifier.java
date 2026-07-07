fdb082844d0a004c3de0ca755336b32805f9bc93
You-Hyuk <cxz6411@naver.com>
2026-07-07 14:18:24 +0900

    [feat] DiscordNotifier 인터페이스 및 구현체 작성

---
+++ src/main/java/com/Coming/Backend/common/discord/DiscordNotifier.java
@@ -0,0 +1,11 @@
+package com.Coming.Backend.common.discord;
+
+import com.Coming.Backend.common.exception.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface DiscordNotifier {
+
+    void notifyFiveXx(HttpServletRequest request, Exception e);
+
+    void notifyFourXx(HttpServletRequest request, ErrorCode errorCode);
+}
